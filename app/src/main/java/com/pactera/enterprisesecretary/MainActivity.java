4566252780f4b4246bec53fcf07edab67cc2eaf2
xiaoyangsavy <xiaoyangsavy@163.com>
2017-04-18 09:57:45 +0800

    test

--- app/src/main/java/com/pactera/enterprisesecretary/MainActivity.java
+++ app/src/main/java/com/pactera/enterprisesecretary/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.print("首页");
+
     }
 }
