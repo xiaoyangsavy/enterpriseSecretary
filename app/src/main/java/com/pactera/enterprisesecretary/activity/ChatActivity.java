9e4170d91958d5820a2e985df4f7743c4a9d2890
xiaoyangsavy <xiaoyangsavy@163.com>
2017-04-24 18:12:54 +0800

    完善聊天界面

--- app/src/main/java/com/pactera/enterprisesecretary/activity/ChatActivity.java
+++ app/src/main/java/com/pactera/enterprisesecretary/activity/ChatActivity.java
@@ -2,11 +2,37 @@ package com.pactera.enterprisesecretary.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import com.pactera.enterprisesecretary.R;
 
+import java.util.List;
+
 //聊天页面
 public class ChatActivity extends MyBaseActivity {
+
+
+    private Button chatMoreButton,chatSendButton = null;
+    private ImageButton chatTypeButton= null;
+    private EditText chatMessageEditText= null;
+    private View chatVoiceButton= null;
+    private RelativeLayout chatMoreView= null;
+    private ScrollView chatMoreScrollView= null;
+    private ImageButton[] moreButtons= null;
+
+    private ListView multiChatList= null;// 信息显示列表
+    private List<Object> messageList= null;
+//    public ChatAdapter chatAdapter= null;
+
+    // 设计师版隐藏布局中的四个按钮
+    private Button button_sendImage, button_phone, button_inviteOrder;// 客户版隐藏布局中的发送图片按钮,拨打电话,邀请查看订单按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
